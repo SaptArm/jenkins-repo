0afd95e47371d0d125d70456da364782a738868c
SaptArm <129938847+SaptArm@users.noreply.github.com>
2023-07-05 20:16:38 +0700

    Update mysqlsript.sh

--- mysqlsript.sh
+++ mysqlsript.sh
@@ -1,3 +1,3 @@
-mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam
-SHOW TABLES;
+mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam \
+  "SHOW TABLES; exit;"
 echo "1"
